b9affbb7f6855dd21c99566d8779d864fa48f8dc
Mariavernia <maversa2@alumni.uv.es>
2022-04-24 11:37:40 +0200

    first commit

---
+++ src/main/java/es/upm/miw/mariavernia/vod/vodspring/VodSpringApplication.java
@@ -0,0 +1,13 @@
+package es.upm.miw.mariavernia.vod.vodspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VodSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VodSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/es/upm/miw/mariavernia/vod/vodspring/VodSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package es.upm.miw.mariavernia.vod.vodspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VodSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
