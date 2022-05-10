88269b1213a879c9c2c411e396b9506342c73b46
Mariavernia <maversa2@alumni.uv.es>
2022-05-10 16:23:06 +0200

    Create role and user models

---
+++ src/main/java/es/upm/miw/mariavernia/vod/vodspring/domain/model/users/Role.java
@@ -0,0 +1,5 @@
+package es.upm.miw.mariavernia.vod.vodspring.domain.model.users;
+
+public enum Role {
+    ADMIN, STUDENT, PROFESSOR
+}
---
+++ src/main/java/es/upm/miw/mariavernia/vod/vodspring/domain/model/users/User.java
@@ -0,0 +1,7 @@
+package es.upm.miw.mariavernia.vod.vodspring.domain.model.users;
+
+public class User {
+    private String email;
+    private String password;
+    private Role role;
+}
