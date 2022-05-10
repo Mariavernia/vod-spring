37683d9dbd010a2598105d1c8371b0e39b630ea2
Mariavernia <maversa2@alumni.uv.es>
2022-05-10 16:49:17 +0200

    fix error

---
+++ src/test/java/es/upm/miw/mariavernia/vod/vodspring/TestConfig.java
@@ -0,0 +1,15 @@
+package es.upm.miw.mariavernia.vod.vodspring;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+@TestPropertySource(locations = "classpath:test.properties")
+@ActiveProfiles("dev")
+public @interface TestConfig {
+
+}
